15a61b881478479e80492a9403b737beb4803701
vaibhavmustilwar <76682158+vaibhavmustilwar@users.noreply.github.com>
2023-08-27 15:51:49 +0530

    Create non_overlapping_testbench.v

---
+++ non_overlapping_testbench.v
@@ -0,0 +1,35 @@
+module TB;
+  reg clk, rst_n, x;
+  wire z;
+  
+  seq_detector_1010 sd(clk, rst_n, x, z);
+  initial clk = 0;   
+  always #2 clk = ~clk;
+    
+  initial begin
+    $monitor("%0t: x = %0b, z = %0b", $time, x, z);
+    x = 0;
+    #1 rst_n = 0;
+    #2 rst_n = 1;
+    
+    #3 x = 1;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #4 x = 1;
+    #4 x = 0;
+    #10;
+    $finish;
+  end
+  
+  initial begin
+    // Dump waves
+    $dumpfile("dump.vcd");
+    $dumpvars(0);
+  end
+endmodule
